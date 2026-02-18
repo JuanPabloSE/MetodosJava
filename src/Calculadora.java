f57ab29169424a6e73f08840d9af8f54819ba8d3
JuanPabloSE <juancontas@protonmail.com>
2026-02-17 23:00:12 -0300

    Commit Message

---
+++ src/Calculadora.java
@@ -0,0 +1,61 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("----- BEM-VINDO A CALCULADORA JAVA -----\n");
+
+        double primeiroValor = lerValor(sc, "DIGITE O PRIMEIRO VALOR: ");
+        double segundoValor = lerValor(sc, "DIGITE O SEGUNDO VALOR: ");
+        String operacao = operacaoLida(sc, "ESCOLHA A OPERAÇÃO DESEJADA (+, -, /, x): ");
+
+        double resultado = valorResultado(primeiroValor, segundoValor, operacao);
+
+        mostrarResultado(resultado);
+
+        sc.close();
+    }
+
+    public static double lerValor(Scanner sc, String mensagem) {
+        System.out.print(mensagem);
+        return sc.nextDouble();
+    }
+
+    private static String operacaoLida(Scanner sc, String mensagem) {
+        System.out.print(mensagem);
+        return sc.next();
+    }
+
+    private static double valorResultado(double primeiroValor, double segundoValor, String operacao) {
+        double aux;
+        switch (operacao) {
+            case "+":
+                aux = primeiroValor + segundoValor;
+                break;
+
+            case "-":
+                aux = primeiroValor - segundoValor;
+                break;
+
+            case "/":
+                aux = primeiroValor / segundoValor;
+                break;
+
+            case "x":
+                aux = primeiroValor * segundoValor;
+                break;
+
+            default:
+                throw new IllegalArgumentException("OPERAÇÃO INCORRETA, TENTE NOVAMENTE!");
+        }
+        return aux;
+    }
+
+    public static void mostrarResultado(double resultado) {
+        System.out.printf("%.1f%n", resultado);
+    }
+
+}
