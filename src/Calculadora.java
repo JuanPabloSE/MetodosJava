2e91f6e2c057a4eaf506c55f02c3ade46e488706
JuanPabloSE <juancontas@protonmail.com>
2026-02-19 19:04:34 -0300

    Commit Message

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -5,16 +5,36 @@ public class Calculadora {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        String opcao;
+        boolean continuar = true;
 
         System.out.println("----- BEM-VINDO A CALCULADORA JAVA -----\n");
 
-        double primeiroValor = lerValor(sc, "DIGITE O PRIMEIRO VALOR: ");
-        double segundoValor = lerValor(sc, "DIGITE O SEGUNDO VALOR: ");
-        String operacao = operacaoLida(sc, "ESCOLHA A OPERAÇÃO DESEJADA (+, -, /, x): ");
+        while (continuar) {
+            try {
+                double primeiroValor = lerValor(sc, "DIGITE O PRIMEIRO VALOR: ");
+                double segundoValor = lerValor(sc, "DIGITE O SEGUNDO VALOR: ");
+                String operacao = operacaoLida(sc, "ESCOLHA A OPERAÇÃO DESEJADA (+, -, /, x): ");
 
-        double resultado = calcularResultado(primeiroValor, segundoValor, operacao);
+                double resultado = calcularResultado(primeiroValor, segundoValor, operacao);
 
-        mostrarResultado(resultado);
+                mostrarResultado(resultado);
+
+                System.out.print("DESEJA CONTINUAR CALCULANDO ? (S/N): ");
+                opcao = sc.next().trim().toUpperCase();
+
+                if (opcao.equals("N")) {
+                    continuar = false;
+                    System.out.println("SISTEMA FINALIZADO. OBRIGADO POR USAR NOSSA CALCULADORA!");
+                }
+            } catch (ArithmeticException e) {
+                System.out.println("\nERRO ❌: " + e.getMessage()); // Receber mensagem de erro personalizada
+            }
+            catch (IllegalArgumentException ex) {
+                System.out.println("\nERRO ❌: " + ex.getMessage()); // Receber mensagem de erro personalizada
+            }
+
+        }
 
         sc.close();
     }
@@ -42,7 +62,7 @@ public class Calculadora {
 
             case "/":
                 if (segundoValor == 0) {
-                    throw new ArithmeticException("Divisão por zero não é permitida!");
+                    throw new ArithmeticException("DIVISÃO POR ZERO NÃO É PERMITIDA!\n");
                 }
                 aux = primeiroValor / segundoValor;
                 break;
@@ -52,7 +72,7 @@ public class Calculadora {
                 break;
 
             default:
-                throw new IllegalArgumentException("OPERAÇÃO INCORRETA, TENTE NOVAMENTE!");
+                throw new IllegalArgumentException("OPERAÇÃO INCORRETA, TENTE NOVAMENTE!\n");
         }
         return aux;
     }
