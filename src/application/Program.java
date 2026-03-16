3606629cc8d25ce3c824b3046cf0043932d3140e
JuanPabloSE <juancontas@protonmail.com>
2026-03-16 13:12:30 -0300

    Commit Message

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,7 +24,6 @@ public class Program {
         System.out.print("DIGITE A OPERAÇÃO DESEJADA ( +, -, /, x ): ");
         calculadora.operacao = sc.next().trim();
 
-
         sc.close();
     }
 
--- src/entities/Calculadora.java
+++ src/entities/Calculadora.java
@@ -40,5 +40,4 @@ public class Calculadora {
         return aux;
     }
 
-
 }
